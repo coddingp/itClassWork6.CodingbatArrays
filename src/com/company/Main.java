49bca2f18860e83ed5a6f4b6dd0eb41458ce5fa0
DotVR46 <=>
2021-12-02 19:52:43 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+import java.util.Arrays;
+public class Main {
+
+    public static void main(String[] args) {
+
+        new int[] nums{1,2,6};
+            return (nums[0] == 6 || nums[nums.length - 1] == 6);
+
+    }
+}
